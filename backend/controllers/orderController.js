6415e9003a522b5ba1d9599767d13aebcc856a8d
goncalofm90 <goncalofm90@gmail.com>
2021-03-23 14:07:24 +0000

    get order by id

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,3 +33,19 @@ export const addOrderItems = asyncHandler(async (req, res) => {
     res.status(201).json(createdOrder);
   }
 });
+
+// Get order by Id
+// GET /api/orders/:id
+// Private route
+export const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found.");
+  }
+});
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router();
 import { protect } from "../middleware/authMiddleware.js";
-import { addOrderItems } from "../controllers/orderController.js";
+import { addOrderItems, getOrderById } from "../controllers/orderController.js";
 
 router.route("/").post(protect, addOrderItems);
+//this should be at the bottom because if you pass anything after it will look at it as an id
+router.route("/:id").get(protect, getOrderById);
 
 export default router;
