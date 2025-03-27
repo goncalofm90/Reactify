def269561a303eca01d22cf27e223d9cd37e7aee
goncalofm90 <goncalofm90@gmail.com>
2025-03-27 19:52:16 +0000

    update db messages

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,12 +5,11 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
     });
 
-    console.log(`mongo db connected: ${conn.connection.host}`.cyan.underline);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error:${error.message}`.red.underline.bold);
+    console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
