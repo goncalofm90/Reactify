4e6bacb5dd19d70d54b2d7a8e026fb276e674b49
goncalofm90 <goncalofm90@gmail.com>
2025-04-02 19:31:15 +0100

    syntax

--- backend/server.js
+++ backend/server.js
@@ -16,24 +16,18 @@ import uploadRoutes from "./routes/uploadRoutes.js";
 dotenv.config();
 connectDB();
 
-// Initialize express app
 const app = express();
 app.use(express.json());
-
-// Enable colors
 colors.enable();
-
-// CORS Middleware
 app.use(
   cors({
-    origin: "https://reactify-ecru.vercel.app", // Allow all origins for testing
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    origin: "https://reactify-ecru.vercel.app",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
-    preflightContinue: false, // Handle preflight requests automatically
+    preflightContinue: false,
   })
 );
 
-// Log CORS for debugging purposes
 app.use((req, res, next) => {
   console.log("CORS Settings Applied");
   console.log("Origin:", req.headers.origin);
@@ -50,23 +44,18 @@ app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
 
-// PayPal configuration endpoint
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-// Static file handling for uploads
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-// Basic API response
 app.get("/", (req, res) => res.send("API is running"));
 
-// Error handling
 app.use(notFound);
 app.use(errorHandler);
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
