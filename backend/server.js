85572ee4d3292dba7439830affa7b45122d5e11c
goncalofm90 <goncalofm90@gmail.com>
2025-04-02 18:48:30 +0100

    api test

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,7 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 //     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 //   });
 // } else {
-//   app.get("/", (req, res) => res.send("API is running"));
+  app.get("/", (req, res) => res.send("API is running"));
 // }
 
 app.use(notFound);
